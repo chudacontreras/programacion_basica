ba5eb2b130bd0f235885917080b0c93cf3431e75
Jesus Contreras Chuda <chudacontreras@gmail.com>
2014-12-23 00:18:28 -0430

    agregado los ciclos parte 2 grilla

--- canvas/js/canvas.js
+++ canvas/js/canvas.js
@@ -7,13 +7,15 @@ function inicio()
 	//alert("Dentro");
 	dibujo = document.getElementById("dibujito");
 	cuadro = dibujo.getContext("2d");
-	cuadro.moveTo(0,0);
+	dibujarGrilla(cuadro);
+
+	/*cuadro.moveTo(0,0);
 	cuadro.lineTo(300,0);
 	cuadro.lineTo(300,300);
 	cuadro.lineTo(0,300);
 	cuadro.lineTo(0,0);
 	cuadro.strokeStyle = "#FF0000";
-	cuadro.stroke();
+	cuadro.stroke();*/
 	
 	/*cuadro.moveTo(100,100);
 	cuadro.lineTo(200,100);
@@ -25,9 +27,38 @@ function inicio()
 	cuadro.strokeStyle = "#0040FF";
 	//para dibujar un arco necesitamos parametros (el centro, el radio, radianes y un valor 
 	//para saber si va con o en contra de las manecillas del reloj)
-	cuadro.arc(150,150,100, (3.1416*2), false);
+	//cuadro.arc(150,150,100, (3.1416*2), false);
+	cuadro.arc(150,150,100, (Math.PI*2), false);
 	//cerramos el path
 	cuadro.closePath();
 	cuadro.stroke();
+
+}
+function dibujarGrilla(l)
+{
+	var ancho = 300, alto =300;
+	var lineas = 10;
+	var linea;
+	var limiteX = ancho / lineas;
+	var limiteY = alto / lineas;
+	for (linea=0; linea<=limiteX; linea++)
+	{
+		l.beginPath();
+		l.strokeStyle ="#AAA";
+		l.moveTo(linea*10, 0);
+		l.lineTo(linea*10,300);
+		l.closePath();
+		l.stroke();
+
+	}
+	for (linea=0; linea<=limiteY; linea++)
+	{
+		l.beginPath();
+		l.strokeStyle ="#AAA";
+		l.moveTo(0, linea*10);
+		l.lineTo(300, linea*10);
+		l.closePath();
+		l.stroke();
+	}
 
 }
